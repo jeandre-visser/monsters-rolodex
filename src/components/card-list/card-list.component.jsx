8b7d799388c8a095e4c30569e19cdd32aa431842
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-30 16:06:03 -0700

    Use destructuring to remove monster dot

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -8,13 +8,16 @@ class CardList extends Component {
     return (
       <div className='card-list'>
         {
-          monsters.map((monster) => (
-            <div className='card-container' key={monster.id}>
-            <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
-            <h2 key={monster.id}>{monster.name}</h2>
-            <p>{monster.email}</p>
-            </div>
-          ))
+          monsters.map((monster) => {
+            const { name, email, id } = monster;
+            return (
+              <div className='card-container' key={id}>
+                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                <h2 key={id}>{name}</h2>
+                <p>{email}</p>
+              </div>
+            )
+          })
         }
       </div>
     )
