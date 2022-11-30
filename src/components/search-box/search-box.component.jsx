351e841077b0340a99c24fcbd87ea292738282d3
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-30 15:46:52 -0700

    Apply styles

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -6,7 +6,7 @@ class SearchBox extends Component {
   render() {
     return (
       <input 
-        className={this.props.className}
+        className={`search-box ${this.props.className}`}
         type="search" 
         placeholder={this.props.placeholder} 
         // this onchange only refers to the onSearchChange method which has already been initialized
