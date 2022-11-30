6f14f634bb1fa431bfccf79e08bcde34897e8e30
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-30 15:26:26 -0700

    Add comment

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,9 +1,8 @@
 import { Component } from 'react';
 
 class CardList extends Component {
-
+// whenever props change or update, then the component will rerender
   render() {
-
     const { monsters } = this.props;
 
     return (
