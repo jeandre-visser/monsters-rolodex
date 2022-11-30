c5ceadabb42ef7c5b3f0179d3f873d93e2759a19
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-29 20:33:18 -0700

    Fetch monsters from api and add console.logs to see when each section of code executes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-
+// classes ALWAYS runs constructor first
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda'
-        },
-        {
-          name: 'Frank'
-        },
-        {
-          name: 'Jack'
-        },
-      ]
+      monsters: []
     }
+    console.log('constructor')
   }
 
+  // after render runs, the component then "mounts" which runs this componentDidMount
+  componentDidMount() {
+    console.log('component')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      }, 
+        () => {
+          console.log(this.state)
+        }
+      ))
+  }
+
+  // the initial render runs after the constructor
   render() {
+    console.log('render')
     return (
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
           })
         }
       </div>
