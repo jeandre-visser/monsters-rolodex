c3b9550fc9da4b2baac1736c9c4b3a06e990e9e4
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-29 19:15:35 -0700

    Add a monsters array and map over to display each monster name in an h1 element

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Jeandre',
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jack'
+        },
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name}
-          </p>
-          <button>
-          Change name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
       </div>
     );
 
